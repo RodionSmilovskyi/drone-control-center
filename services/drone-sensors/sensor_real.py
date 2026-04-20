27f785773ee4a4e887f29f0e7509bb1fc1feedc8
Rodion Smilovskyi <smelovsky86@gmail.com>
2026-04-19 20:35:53 -0400

    Continue calibration

--- services/drone-sensors/sensor_real.py
+++ services/drone-sensors/sensor_real.py
@@ -36,7 +36,7 @@ class SensorReal:
         self.last_time = time.time()
 
         # Normalization and Calibration
-        self.FLOW_SCALAR = 0.027
+        self.FLOW_SCALAR = 0.084
         self.MAX_VELOCITY = 5.0
         self.MAX_XY_SHIFT = 1.0
         self.MAX_ALTITUDE = 1.0
